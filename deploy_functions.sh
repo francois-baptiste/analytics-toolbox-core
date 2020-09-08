964d3959a1a56c0e7a2a9c2ce9f88a51e170625a
François BAPTISTE <francois.baptiste@noreply.com>
2020-09-08 17:54:59 +0200

    change target

--- deploy_functions.sh
+++ deploy_functions.sh
@@ -11,13 +11,8 @@ find "$(pwd)" -name "*.sql" | sort  -z |while read fname; do
   file_name=$(basename "${fname}")
   function_name="${file_name%.*}"
 
-  #we iterate over the regions to update or create all functions in the different regions
-  for reg in "${regions[@]}"
-  do
-
 
     bq --project_id libjs4eu query --use_legacy_sql=false --flagfile=$fname
 
-  done
 done
 
